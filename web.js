94a65429e5ebd78cb0d69eb9c2f13f058e11a261
Nick Drake <ntldrake@gmail.com>
2013-07-09 06:24:18 +0000

    Display text pulled from index. Attempt 2

--- web.js
+++ web.js
@@ -1,11 +1,14 @@
+
 var express = require('express');
 
-var file_content = fs.readFile('index.html', 'utf-8');
+var file_content = fs.readFile('~/bitstarter/index.html', 'utf-8');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(file_content);
+  var fs = require('fs');
+  var buffer = new buffer();
+  response.send(buffer.toString('utf8', fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
