6c433c4f8bcf83a2ff91e4c2385536081b6632a2
Nick Drake <ntldrake@gmail.com>
2013-07-09 05:52:12 +0000

    Display text pulled from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
+var file_content = fs.readFile('index.html', 'utf-8');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(file_content);
 });
 
 var port = process.env.PORT || 5000;
